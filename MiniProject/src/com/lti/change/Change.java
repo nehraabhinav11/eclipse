0828621e86cf24c7a80251c13fb1cccfff987918
abhinav <abhinavnehra11@gmail.com>
2019-07-25 10:05:04 -0700

    change in change.java

--- MiniProject/src/com/lti/change/Change.java
+++ MiniProject/src/com/lti/change/Change.java
@@ -5,6 +5,7 @@ public class Change {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 				System.out.println("CHange the project!!");
+				System.out.println("Change again!!");
 	}
 
 }
