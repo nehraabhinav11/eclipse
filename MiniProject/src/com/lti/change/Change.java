a38f7e957a2f7c22bd8b7d7effcc58b356888250
abhinav <abhinavnehra11@gmail.com>
2019-07-25 09:54:51 -0700

    change class inserted.

---
+++ MiniProject/src/com/lti/change/Change.java
@@ -0,0 +1,10 @@
+package com.lti.change;
+
+public class Change {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				System.out.println("CHange the project!!");
+	}
+
+}
